256d00b16d60e9375bf37c0bf30864daafe50b27
Naveen Singh <naveensingh.org@gmail.com>
2015-07-25 21:31:54 +0530

    initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name naveensinghApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the naveensinghApp
+ */
+angular.module('naveensinghApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name naveensinghApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the naveensinghApp
+ */
+angular.module('naveensinghApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
